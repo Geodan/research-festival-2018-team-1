a21b571afe9afad49660602f1a5d91b33a283bd4
bert <btemme@hotmail.com>
2018-10-04 17:27:49 +0200

    add mqtt + compass sample

---
+++ src/steward/App1/App1/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using uPLibrary.Networking.M2Mqtt;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        private double headingNorth;
+        private MqttClient client;
+        string topic = "arena/test";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // start compass
+            Compass.Start(SensorSpeed.UI);
+            Compass.ReadingChanged += Compass_ReadingChanged;
+
+            var broker = "broker.hivemq.com";
+            var port = 1883;
+
+            client = new MqttClient(broker);
+            var clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+        }
+
+
+        void Compass_ReadingChanged(object sender, CompassChangedEventArgs e)
+        {
+            var data = e.Reading;
+            headingNorth = data.HeadingMagneticNorth;
+            Debug.WriteLine("Headingnorth:" + headingNorth);
+            client.Publish(topic, Encoding.Default.GetBytes(headingNorth.ToString()));
+        }
+    }
+}
