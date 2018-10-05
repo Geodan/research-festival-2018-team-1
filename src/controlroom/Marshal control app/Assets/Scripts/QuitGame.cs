d2adde4caa32c2f79ffc5a5201f4d88c20277bdc
Steven K <stevenkuperus@gmail.com>
2018-10-05 15:23:35 +0200

    Update: added Quit app function

---
+++ src/controlroom/Marshal control app/Assets/Scripts/QuitGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Quit();
+    }
+
+    void Quit()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            Application.Quit();
+        }
+    }
+}
