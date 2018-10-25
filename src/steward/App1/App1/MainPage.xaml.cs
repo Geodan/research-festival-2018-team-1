3c9fb3b0f96e4edf178a4b588a6abb21d13208c6
bert <btemme@hotmail.com>
2018-10-25 16:55:04 +0200

    retryconnect

--- src/steward/App1/App1/MainPage.xaml.cs
+++ src/steward/App1/App1/MainPage.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using Plugin.Geolocator;
 using Plugin.Permissions.Abstractions;
 using System;
+using System.Diagnostics;
 using System.Text;
+using System.Threading.Tasks;
 using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Exceptions;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
@@ -18,11 +21,12 @@ namespace App1
 
         private MqttClient client;
         private string broker = "iot.eclipse.org";
-        private int publishInterval = 1;
+        private int publishInterval = 10;
         private IWifiIp wifiIp;
         private string deviceId;
         private int numberOfMesssages = 0;
         private bool haslocation = false;
+        private string oldip;
 
         public MainPage()
         {
@@ -68,6 +72,27 @@ namespace App1
             StartPublishing();
         }
 
+        private void RetryConnect()
+        {
+            var clientId = Guid.NewGuid().ToString();
+
+            for (var n = 0; n < 50; n++)
+            {
+                try
+                {
+                    client.Connect(clientId);
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("Ex:" + ex);
+                }
+                Task.Delay(TimeSpan.FromSeconds(1)).Wait();
+            }
+
+
+        }
+
         public void StartPublishing()
         {
             client = new MqttClient(broker);
@@ -79,13 +104,24 @@ namespace App1
                 if (haslocation)
                 {
                     var ip = wifiIp.GetWifiIp();
-                    var message = $"{longitude},{latitude},{Math.Round(accuracy, 0)},{Math.Round(headingMagneticNorth, 0)},{ip},{name}";
+
+                    // IP changed?
+                    if (oldip != null && ip!=oldip)
+                    {
+                        RetryConnect();
+                    }
+
+                    var dt = DateTime.UtcNow.ToString("o");
+                    var message = $"{longitude},{latitude},{Math.Round(accuracy, 0)},{Math.Round(headingMagneticNorth, 0)},{ip},{name},{dt}";
                     client.Publish($"arena/{deviceId}", Encoding.Default.GetBytes(message));
+
                     numberOfMesssages++;
                     Device.BeginInvokeOnMainThread(() =>
                     {
+                        TxtIP.Text = ip;
                         TxtNumberOfMessages.Text = numberOfMesssages.ToString();
                     });
+                    oldip = ip;
                 }
                 return true; // True = Repeat again, False = Stop the timer
             });
